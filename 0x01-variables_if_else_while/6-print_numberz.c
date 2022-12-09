35cb1b981669d5e5cf7b02304703dcda5836e534
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-09 03:59:04 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print numbers from zero to ten using putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar("\n");
+	return (0);
+}
