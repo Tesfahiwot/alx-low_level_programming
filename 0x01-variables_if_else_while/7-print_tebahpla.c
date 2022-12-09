34bbd001950019a154d1b53af39f2165e0becd32
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-09 05:18:15 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - decreasing alphabet
+ * return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
