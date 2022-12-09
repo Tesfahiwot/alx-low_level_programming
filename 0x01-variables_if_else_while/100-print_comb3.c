8d563e1a3db1f1da515ce94b7fa726d2338ce777
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-09 12:07:05 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - prints all possible differe
+ *Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (x = n + 1; x <= 9; x++)
+		{
+		putchar(n + '0');
+		putchar(x + '0');
+		if (x < 8)
+		{
+			putchar(',');
+			putchar(' ')
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
