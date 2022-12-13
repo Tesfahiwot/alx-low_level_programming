278abf4af4dec23ab26775346936057be513f2ac
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-13 04:40:04 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - main function 
- * Return: Always 0
+ *main - check the code for Holberton School students.
+ *Return: Always 0
  */
+
 int main(void)
+
 {
-	int j;
 
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
-	_putchar('\n');
+		print_alphabet();
+
+			return (0);
+
 }
-
