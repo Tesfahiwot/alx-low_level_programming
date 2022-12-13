633d6ead9f93a6e38f6c2d307777127a7f5670e8
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-13 04:59:59 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * main - prints the word putchar
+ * main - prints putchar
  * Return: Always 0
  */
-int main(void)
+
+int _putchar(char c)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	
-	return (0);
+
+		return (write(1, &c, 1));
+
 }
