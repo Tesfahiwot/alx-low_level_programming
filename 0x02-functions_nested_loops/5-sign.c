7a4e03f04bcb6230ca6a70bb6f8bee87c9f837f0
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-13 08:07:24 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints signs
+ * @n: an argument
+ * Return: 1, 0, -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_Putchar('+');
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_Putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return ('-');
+	}
+}
