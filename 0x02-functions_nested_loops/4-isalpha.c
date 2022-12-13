909a008006ba9b65380e0c2e37475cc8e77ec469
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-13 07:40:37 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checkes for alphabetical letter
- * @c: a character to be checked 
+ * @c: a character to be checked
  * Return: returns 0or 1
  */
 int _isalpha(int c)
