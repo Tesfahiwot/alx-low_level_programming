bcb07a237f2e21e9cd9be5ad06cc6a32f21a8cfe
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-14 02:27:44 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,10 +7,10 @@
 void print_to_98(int n)
 {
 
-	for (n = 0; n <= 98; n++)
-	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-	}
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
 }
