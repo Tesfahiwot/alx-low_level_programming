e4d01f82836f5348829a0ae2817de0d2acc14c50
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-14 02:09:26 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural number from n to 98
+ * @n: an integer
+ * Return: alwyas 0
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	for (x = n, x <= 98; x++)
+	{
+		_putchar(x + '0');
+	}
+}
