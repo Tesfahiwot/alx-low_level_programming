d74e8b2bcd905ead4ed14755a57da7a3aa40ddc8
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-13 22:24:53 -0800

    task 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,9 +17,9 @@ void jack_bauer(void)
 				{
 					_putchar(a + '0');
 					_putchar(b + '0');
+					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
-					_putchar(':');
 					_putchar('\n');
 				}
 			}
