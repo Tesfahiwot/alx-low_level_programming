08559fdb93b2b0b05e6045f1752d7b883aeada0a
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-15 05:03:47 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,6 +8,13 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+	{
+		if ((i = 2) || (i == 4))
+			continue;
+			else
+			{
+				_putchar(i + '0');
+			}
+			_putchar('\n');
+	}
 }
