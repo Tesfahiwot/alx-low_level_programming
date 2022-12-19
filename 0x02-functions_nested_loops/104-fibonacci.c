33d3e0fdc3c4219bc496f7fa80f3c326f578740b
Tesfahiwot <117745979+Tesfahiwot@users.noreply.github.com>
2022-12-19 14:37:52 +0300

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 /**
- * main - main fuction
- * Return: Always 0
- */
+* main - print the first 98 fibonacci numbers.
+* Return: Nothing.
+*/
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
-	short int i = 1, initial0s;
-
-	while (i <= 98)
-	{
-		if (f10 > 0)
-			printf("%lu", f10);
-		initial0s = mx - 1 - f1;
-		while (f10 > 0 && initial0s > 0)
-		{
-			printf("%i", 0);
-			initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f10 + f20 + (f1 + f2) / mx;
-		f1 = f2;
-		f10 = f20;
-		f2 = tmp;
-		f20 = tmpo;
-
-		if (i != 98)
-			printf(",");
-		else
-			printf("\n");
-		i++;
-	}
-	return (0);
+int count;
+unsigned long i, j, k;
+unsigned long m, n, p, carry;
+count = 0;
+i = 0;
+j = 1;
+for (count = 1; count <= 91; count++)
+{
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
+}
+m = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (count <= 98)
+{
+carry = (m + n) / 1000;
+p = (m + n) -carry * 1000;
+k = (i + j) +carry;
+m = n;
+n = p;
+i = j;
+j = k;
+if (p >= 100)
+printf("%lu%lu", k, p);
+else
+printf("%lu0%lu", k, p);
+if (count != 98)
+printf(", ");
+count++;
+}
+putchar('\n');
+return (0);
 }
