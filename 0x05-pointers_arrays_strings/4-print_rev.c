6505feb6fa798e729184ff3e96a731aafee1aff1
Tesfahiwot <tesfahiwot135@gmail.com>
2022-12-19 02:22:45 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "malin.h"
+/**
+ * _rev - prints a string in a reverse followed by a new line
+ * @s: char input
+ * Return: character
+ */
+void print_rev(char *s)
+{
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
