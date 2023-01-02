e4dc9aa6ad01f48ebb2be3489a188098c8b999ec
Tesfahiwot <117745979+Tesfahiwot@users.noreply.github.com>
2023-01-02 15:53:44 +0300

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,16 +7,15 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (s[i] == accept[n])
-				return (s + i);
-		}
-	}
-
-	return (NULL);
+for (i = 0; *(s + i) != '\0'; i++)
+{
+for (j = 0; *(accept + j) != '\0'; j++)
+{
+if (*(s + i) == *(accept + j))
+return (s + i);
+}
+}
+return ('\0');
 }
